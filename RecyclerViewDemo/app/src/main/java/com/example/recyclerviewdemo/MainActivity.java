195d12eb88b52356dc3c14177d424c97bb4c3846
shrawani0808 <shrawanidharmadhikari@gmail.com>
2026-06-27 17:57:26 +0530

    Edited MainActivity.java

--- RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
+++ RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
@@ -7,8 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recyclerviewdemo.adapters.CourceAdapter;
+import com.example.recyclerviewdemo.models.CourceModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
+    private RecyclerView recyclerview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initComp();
+
+        getAllList();
+    }
+
+    private void getAllList(){
+        ArrayList<CourceModel> courceList = new ArrayList<>();
+        CourceModel model = new CourceModel();
+        model.setCourceTitle("C Language");
+        model.setCourcePrice("299");
+        CourceModel model1 = new CourceModel();
+        model1.setCourceTitle("C++ Language");
+        model1.setCourcePrice("399");
+        courceList.add(model);
+        courceList.add(model1);
+        CourceAdapter adapter = new CourceAdapter(MainActivity.this,courceList);
+        recyclerview.setAdapter(adapter);
+
+    }
+    public void initComp(){
+        fab= findViewById(R.id.fabAdd);
+        recyclerview=findViewById(R.id.courseRV);
+        LinearLayoutManager layoutManager= new LinearLayoutManager(MainActivity.this);
+        recyclerview.setLayoutManager(layoutManager);
     }
 }
