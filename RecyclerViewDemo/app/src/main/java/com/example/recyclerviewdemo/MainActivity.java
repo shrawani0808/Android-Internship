c5c1392c56c747a96041aa1ab1e58977d1e6ff25
shrawani0808 <shrawanidharmadhikari@gmail.com>
2026-06-27 18:16:11 +0530

    fixed courcelayout and added image dependencies

--- RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
+++ RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
@@ -38,14 +38,18 @@ public class MainActivity extends AppCompatActivity {
 
     private void getAllList(){
         ArrayList<CourceModel> courceList = new ArrayList<>();
-        CourceModel model = new CourceModel();
-        model.setCourceTitle("C Language");
-        model.setCourcePrice("299");
-        CourceModel model1 = new CourceModel();
-        model1.setCourceTitle("C++ Language");
-        model1.setCourcePrice("399");
-        courceList.add(model);
-        courceList.add(model1);
+//        CourceModel model = new CourceModel();
+//        model.setCourceTitle("C Language");
+//        model.setCourcePrice("299");
+//        CourceModel model1 = new CourceModel();
+//        model1.setCourceTitle("C++ Language");
+//        model1.setCourcePrice("399");
+//        courceList.add(model);
+//        courceList.add(model1);
+
+        courceList.add(new CourceModel("https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/1920px-C_Programming_Language.svg.png","C Language","299"));
+        courceList.add(new CourceModel("https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1280px-ISO_C%2B%2B_Logo.svg.png","C++ Language","399"));
+        courceList.add(new CourceModel("https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/960px-Java_programming_language_logo.svg.png","Java Language","499"));
         CourceAdapter adapter = new CourceAdapter(MainActivity.this,courceList);
         recyclerview.setAdapter(adapter);
 
--- RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/adapters/CourceAdapter.java
+++ RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/adapters/CourceAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.recyclerviewdemo.R;
 import com.example.recyclerviewdemo.holders.CourceViewHolder;
 import com.example.recyclerviewdemo.models.CourceModel;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -36,9 +38,14 @@ public class CourceAdapter extends RecyclerView.Adapter<CourceViewHolder> {
     public void onBindViewHolder(@NonNull CourceViewHolder holder, int position) {
             String title = courceList.get(position).getCourceTitle();
             String price = courceList.get(position).getCourcePrice();
-            //String image = courceList.get(position).getCourceImage();
+            String image = courceList.get(position).getCourceImage();
             holder.tvTitle.setText(title);
             holder.tvPrice.setText(price);
+            try{
+                Picasso.get().load(image).placeholder(R.drawable.ic_launcher_background).into(holder.courceIv);
+            } catch (Exception e) {
+                Toast.makeText(context, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
     }
 
     @Override
