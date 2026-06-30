9a1fbabda2b709fd330cfa139059e7fd24af33e1
shrawani0808 <shrawanidharmadhikari@gmail.com>
2026-06-30 16:44:08 +0530

    mearged edit and delete functions

--- RecyclerViewEx/app/src/main/java/com/example/recyclerviewex/adapters/MyAdapter.java
+++ RecyclerViewEx/app/src/main/java/com/example/recyclerviewex/adapters/MyAdapter.java
@@ -1,11 +1,17 @@
 package com.example.recyclerviewex.adapters;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.recyclerviewex.R;
@@ -28,18 +34,73 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(context).inflate(R.layout.name_layout,parent,false);
-        MyViewHolder myholder = new MyViewHolder(v);
-        return myholder;
+        return new MyViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        String name = nameList.get(position).getName();
+        Model model = nameList.get(position);
+        String name = model.getName();
         holder.myname.setText(name);
+
+        holder.itemView.setOnClickListener(v -> {
+            openDialog(model,position);
+        });
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Confirmation Window");
+                builder.setMessage("Do you want to delete the name?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        nameList.remove(position);
+                        notifyItemRemoved(position);
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                return true;
+            }
+        });
+
     }
 
     @Override
     public int getItemCount() {
         return nameList.size();
     }
+
+    public void openDialog(Model model,int position){
+        Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.add_name_layout);
+        TextView nameTv = dialog.findViewById(R.id.edtname);
+        Button btnEditAndSave = dialog.findViewById(R.id.btnEditAndSave);
+
+        nameTv.setText(model.getName());
+        btnEditAndSave.setText("Edit");
+
+        btnEditAndSave.setOnClickListener(v -> {
+            String name = nameTv.getText().toString();
+            Model newmodel = new Model(name);
+            nameList.add(position,newmodel);
+            notifyItemChanged(position);
+            dialog.dismiss();
+        });
+        dialog.show();
+
+
+    }
+
 }
