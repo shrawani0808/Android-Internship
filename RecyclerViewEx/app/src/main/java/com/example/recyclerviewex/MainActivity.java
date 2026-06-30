df3ac339456c3cc2f87b85049eea284dca998c2b
shrawani0808 <shrawanidharmadhikari@gmail.com>
2026-06-30 17:28:37 +0530

    completed crud operations

--- RecyclerViewEx/app/src/main/java/com/example/recyclerviewex/MainActivity.java
+++ RecyclerViewEx/app/src/main/java/com/example/recyclerviewex/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.recyclerviewex;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,11 +15,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.recyclerviewex.adapters.MyAdapter;
 import com.example.recyclerviewex.models.Model;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FloatingActionButton fab;
     private TextView nameTv;
     private RecyclerView recyclerView ;
     ArrayList<Model> nameList = new ArrayList<>();
@@ -38,15 +42,36 @@ public class MainActivity extends AppCompatActivity {
         initComp();
 
         nameList.add(new Model("Shrawani"));
-        nameList.add(new Model("Shrawani"));
-        nameList.add(new Model("Shrawani"));
-        nameList.add(new Model("Shrawani"));
+        nameList.add(new Model("Sharwari"));
+        nameList.add(new Model("Tejal"));
+        nameList.add(new Model("Shraddha"));
+        nameList.add(new Model("Sayali"));
+        nameList.add(new Model("Rutuja"));
+        nameList.add(new Model("Vaishnavi"));
         adapter= new MyAdapter(MainActivity.this,nameList);
         recyclerView.setAdapter(adapter);
+
+        fab.setOnClickListener(v -> {
+            Dialog dialog = new Dialog(MainActivity.this);
+            dialog.setContentView(R.layout.add_name_layout);
+            Button btnEditAndSave = dialog.findViewById(R.id.btnEditAndSave);
+
+            btnEditAndSave.setText("Save");
+            btnEditAndSave.setOnClickListener(v1 -> {
+                TextView nameTextview = dialog.findViewById(R.id.nameTv);
+                String name = nameTextview.getText().toString();
+                Model model = new Model(name);
+                nameList.add(model);
+                adapter.notifyItemInserted(nameList.size());
+                dialog.dismiss();
+            });
+            dialog.show();
+        });
 
     }
 
     public void initComp(){
+        fab = findViewById(R.id.fab);
         nameTv = findViewById(R.id.nameTv);
         recyclerView=findViewById(R.id.myRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
